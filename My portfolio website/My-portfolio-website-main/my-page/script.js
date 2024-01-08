d48a90c5507ad432afcae4565e1dbd93f916b4f1
IT-BPs <153557805+IT-BPs@users.noreply.github.com>
2024-01-08 16:33:03 +0100

    Add files via upload

---
+++ My portfolio website/My-portfolio-website-main/my-page/script.js
@@ -0,0 +1,52 @@
+// chrome.runtime.onMessage.addListener(function(rq, sender, sendResponse)
+// {
+//     setTimeout(function() {
+//         sendResponse({status: true});
+//     }, 1);
+//     return true;  // Add return true to fix the error.
+// });
+if (typeof window !== 'undefined') {
+    console.log('You are on the browser')
+} else {
+    console.log('You are on the server')
+}
+
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll('section');
+const navbarLinks = document.querySelectorAll('.navbar-link');
+const progress = document.querySelector('.progress-bars-wrapper');
+const progressBarPercents =[60,50,40,30,20,20];
+
+
+window.addEventListener("scroll", () => {
+mainFn();
+});
+const mainFn = () => {    if (window.pageYOffset >= navbar.offsetTop) {
+
+    navbar.classList.add('sticky')
+} else {
+    navbar.classList.remove('sticky');
+}
+    sections.forEach((section, i )=> {
+        if (window.pageYOffset >= section.offsetTop -10){
+            navbarLinks.forEach(navbarLink => {
+                navbarLink.classList.remove('change');
+            });
+            navbarLinks[i].classList.add("change");
+        }
+    });
+
+    if (window.pageYOffset + window.innerHeight >= progress.offsetTop){
+        document.querySelectorAll(".progress-percent").forEach((el,i) => {
+            el.style.width = `${progressBarPercents[i]}%`;
+            el.previousElementSibling.firstElementChild.textContent = progressBarPercents[i];
+        });
+    }
+}
+mainFn();
+
+// window.addEventListener('resize', () => {
+//     window.location.reload();
+// });
+
